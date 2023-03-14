51bed2a5563ea6d6375eaaddb2e60fa6e0827740
Mohamed Islem Ayari <mohamed-islem.ayari@cloudpilots.com>
2023-03-14 21:30:08 +0100

    first

---
+++ vite.config.ts
@@ -0,0 +1,9 @@
+/* Copyright (c) 2023 CLOUDPILOTS Software & Consulting GmbH */
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+});
